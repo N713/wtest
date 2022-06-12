fb312ce1862c8e1e5c783c0d3be234e74023f866
nmvil <naumovely@cryp.im>
2022-06-12 20:09:33 +0300

    1366px & PP

--- source/js/components/swiper.js
+++ source/js/components/swiper.js
@@ -5,18 +5,37 @@ let numberOfSlides = 4;
 
 const setSwiper = () => {
   const swiper = new Swiper('.swiper', {
-    slidesPerView: numberOfSlides,
-    slidesPerGroup: numberOfSlides,
-    spaceBetween: GAP,
-  
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: 'true',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      slidesPerView: numberOfSlides - 3,
+      slidesPerGroup: numberOfSlides - 3,
+      spaceBetween: GAP - 20,
+    
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: 'true',
+      },
+    
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
+      1366: {
+        slidesPerView: 'auto',
+        slidesPerGroup: numberOfSlides - 1,
+        spaceBetween: GAP + 28
+      },
+
+      1920: {
+        slidesPerView: numberOfSlides,
+        slidesPerGroup: numberOfSlides,
+        spaceBetween: GAP
+      }
     }
   });
 }
